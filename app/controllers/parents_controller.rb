cbea0de90570c93ec06ff9de0c49f30b8416b044
tanizawa_kenta <kt10025102@gmail.com>
2019-11-26 11:44:00 +0900

    added:scaffold

---
+++ app/controllers/parents_controller.rb
@@ -0,0 +1,60 @@
+class ParentsController < ApplicationController
+  before_action :set_parent, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @parents = Parent.all
+  end
+
+  def show
+  end
+
+  def new
+    @parent = Parent.new
+  end
+
+  def edit
+  end
+
+  def create
+    @parent = Parent.new(parent_params)
+
+    respond_to do |format|
+      if @parent.save
+        format.html { redirect_to @parent, notice: 'Parent was successfully created.' }
+        format.json { render :show, status: :created, location: @parent }
+      else
+        format.html { render :new }
+        format.json { render json: @parent.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @parent.update(parent_params)
+        format.html { redirect_to @parent, notice: 'Parent was successfully updated.' }
+        format.json { render :show, status: :ok, location: @parent }
+      else
+        format.html { render :edit }
+        format.json { render json: @parent.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @parent.destroy
+    respond_to do |format|
+      format.html { redirect_to parents_url, notice: 'Parent was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    def set_parent
+      @parent = Parent.find(params[:id])
+    end
+
+    def parent_params
+      params.require(:parent).permit(:name, :age)
+    end
+end
